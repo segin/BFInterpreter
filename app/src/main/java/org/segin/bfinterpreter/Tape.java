74ed04e16ed33b889b336cd40ec0db56171be3e0
Kirn Gill II <segin2005@gmail.com>
2014-07-27 20:12:49 -0500

    filled in the rest of the tape code

--- app/src/main/java/org/segin/bfinterpreter/Tape.java
+++ app/src/main/java/org/segin/bfinterpreter/Tape.java
@@ -4,27 +4,59 @@ package org.segin.bfinterpreter;
  * Created by Kirn on 7/27/14.
  */
 public class Tape {
-    private byte[] tape;
-    private short position;
+    private char[] tape;
+    private int position;
 
     public Tape() {
-        tape = new byte[0x10000];
+        tape = new char[0x10000];
         position = 0;
     }
 
-    public int get() {
+    public char get() {
         return tape[position];
     }
 
-    public void set(short position) {
+    public void set(char value) {
+        tape[position] = value;
+    }
+
+    public void setPosition(int position) {
         this.position = position;
     }
 
+    public int getPosition() {
+        return position;
+    }
+
     public void inc() {
-        tape[position]++;
+        if (tape[position] >= 255) {
+            tape[position] = 0;
+        } else {
+            tape[position]++;
+        }
     }
 
     public void dec() {
-        tape[position]--;
+        if (tape[position] == 0) {
+            tape[position] = 255;
+        } else {
+            tape[position]++;
+        }
+    }
+
+    public void forward() {
+        if (position >= 65535) {
+            position = 0;
+        } else {
+            position++;
+        }
+    }
+
+    public void reverse() {
+        if (position <= 0) {
+            position = 65535;
+        } else {
+            position++;
+        }
     }
 }
