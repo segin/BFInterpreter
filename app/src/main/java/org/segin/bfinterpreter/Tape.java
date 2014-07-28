6884b568c1dca00d09952a20175769caff7d54a4
Kirn Gill II <segin2005@gmail.com>
2014-07-27 23:37:47 -0500

    License header blocks added

--- app/src/main/java/org/segin/bfinterpreter/Tape.java
+++ app/src/main/java/org/segin/bfinterpreter/Tape.java
@@ -1,8 +1,21 @@
 package org.segin.bfinterpreter;
 
-/**
- * Created by Kirn on 7/27/14.
+/*
+ * Copyright 2014 Kirn Gill II <segin2005@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 public class Tape {
     private char[] tape;
     private int position;
@@ -18,14 +31,6 @@ public class Tape {
 
     public void set(char value) {
         tape[position] = value;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public int getPosition() {
-        return position;
     }
 
     public void inc() {
